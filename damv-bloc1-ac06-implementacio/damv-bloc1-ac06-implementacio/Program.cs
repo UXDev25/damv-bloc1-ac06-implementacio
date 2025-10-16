02afeddcfc08fa7fefcc93bf78c60d3af910342d
Unai Almiñana <unai.alminana.7e9@itb.cat>
2025-10-16 11:55:05 +0200

    Update Program.cs

--- damv-bloc1-ac06-implementacio/damv-bloc1-ac06-implementacio/Program.cs
+++ damv-bloc1-ac06-implementacio/damv-bloc1-ac06-implementacio/Program.cs
@@ -6,17 +6,17 @@ public class Program
 {
     public static void Main() 
     { 
-        const string MSG_INPUT1 = "Enter the first integer:";
-        const string MSG_INPUT2 = "Enter the second integer:";
-        const string MSG_INPUT3 = "The sum is: ";
+        const string Msg_Input1 = "Enter the first integer:";
+        const string Msg_Input2 = "Enter the second integer:";
+        const string Msg_Input3 = "The sum is: ";
         int num1, num2, sum;
 
-        Console.WriteLine(MSG_INPUT1);
+        Console.WriteLine(Msg_Input1);
         num1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(MSG_INPUT2);
+        Console.WriteLine(Msg_Input2);
         num2 = Convert.ToInt32(Console.ReadLine());
         sum = num1 + num2;
-        Console.WriteLine(MSG_INPUT3 + sum);
+        Console.WriteLine(Msg_Input3 + sum);
     }
 }
 // POSTCONDITION: 'sum' contains the sum of 'num1' and 'num2', and it is displayed.
