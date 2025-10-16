feb712fcf2b1e6f371e234e0aa46729d51864891
Unai Almiñana <unai.alminana.7e9@itb.cat>
2025-10-16 10:36:26 +0200

    Create HelloWorld

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace helloWorld
+{
+    public class hello
+    {
+        public static void Main()
+        { 
+            const string Msg = "¡Hola mundo!"
+            Console.WriteLine(Msg);
+        }
+    }
+}
+
