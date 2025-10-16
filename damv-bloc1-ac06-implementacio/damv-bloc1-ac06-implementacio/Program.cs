3f58ae41342e6690d8172755bb6cf61a480c123d
Unai Almiñana <unai.alminana.7e9@itb.cat>
2025-10-16 10:46:32 +0200

    Solution Exercise 1

--- damv-bloc1-ac06-implementacio/damv-bloc1-ac06-implementacio/Program.cs
+++ damv-bloc1-ac06-implementacio/damv-bloc1-ac06-implementacio/Program.cs
@@ -4,8 +4,9 @@ public class Program
 {
     public static void Main()
     {
-         const string Msg = "¡Hola mundo!";
-         Console.WriteLine(Msg);
+        const string MSG_INPUT1 = "Enter the first integer:"
+        const string MSG_INPUT2 = "Enter the second integer:"
+        const string MSG_INPUT3 = "The sum is:"
     }
 }
 
