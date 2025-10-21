12d308585ac6b65c8a04eb4b48946e23281490fc
Unai Almiñana <unai.alminana.7e9@itb.cat>
2025-10-21 12:58:37 +0200

    Solution of The pseudocode ExerciseThree

--- damv-bloc1-ac06-implementacio/damv-bloc1-ac06-implementacio/Program.cs
+++ damv-bloc1-ac06-implementacio/damv-bloc1-ac06-implementacio/Program.cs
@@ -2,32 +2,54 @@
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 
-// PRECONDITION: The algorithm is ready to accept any integer input value.
+// PRECONDITION: The user must be ready to input integer values when prompted.
 using System;
 public class Program
 {
     public static void Main() 
     {
-        const string msgInput = "Enter a value to check if it's within the interval (5, 50):";
-        const string msgOk = " is within the interval (5, 50).";
-        const string msgKo = " is NOT within the interval (5, 50).";
+        const string msgInput = "Enter an integer number within the interval (2, 30):";
+        const string msgNumAttemps = "Attempt of 3: ";
+        const string msgOk = "Valid number entered: ";
+        const string msgKo = "Error: The number is not within the interval (2, 30). It must be between 3 and 29 (inclusive).";
+        const string msgLost = "You have lost. No valid number entered.";
+        const int maxAttemps = 3;
+        const int minInt = 2;
+        const int maxInt = 30;
 
-        int number;
+        int number; 
+        int attemps = 1;
+        bool isValid = false;
 
-        Console.WriteLine(msgInput);
-        number = Convert.ToInt32(Console.ReadLine());
-
-        if (number > 5 && number < 50) 
+        while (attemps <= maxAttemps && !isValid) 
         {
-            Console.WriteLine(msgOk); ;
+            Console.WriteLine(msgNumAttemps, attemps);
+            Console.WriteLine(msgInput);
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > minInt && number < maxInt)
+            {
+                isValid = true;
+            }
+            else 
+            {
+                Console.WriteLine(msgKo);
+                attemps++;
+            }
         }
-        else
+
+        if (isValid)
         {
-            Console.WriteLine(msgKo);
+            Console.WriteLine(msgOk, number);
         }
+        else 
+        {
+            Console.WriteLine(msgLost);
+        }
+
     }
 }
-// POSTCONDITION: The algorithm has read a 'number' and output a message indicating whether 'number' is strictly between 5 and 50.
+// POSTCONDITION: Either 'isValid' is TRUE and a valid 'number' (3-29) has been read, or 'attempts' equals 4 and the user is informed of failure.
 
 
 
